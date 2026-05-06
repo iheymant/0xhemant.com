3cfba578ef729e479fb713d1da1e8cb44c93e8a1
iheymant <Hemant.kaliyar@gmail.com>
2026-05-06 11:31:33 +0530

    add header, mobile menu, sections

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('.logo')
+let mobileMenu = document.querySelector('.mobile-menu')
+
+
+logo.addEventListener('click', function (){
+  mobileMenu.style.display = 'flex'
+  
+})
