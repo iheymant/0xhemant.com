832ab8d451e8096550f510ce4c073c716f971ce8
iheymant <Hemant.kaliyar@gmail.com>
2026-08-17 15:41:44 +0530

    move to dedicated path

---
+++ js/builds/voicekeep/voicekeep.js
@@ -0,0 +1,50 @@
+import { buildsProjectsData } from '/data/builds/index.js';
+import { voicekeepContents } from '/data/builds/voicekeep/content.js';
+import { voicekeepScreenshots } from '/data/builds/voicekeep/screenshots.js';
+
+import { createBuildHeroMarkup } from '/js/components/builds/render-builds/create-build-hero-markup.js';
+import { renderLinks } from '/js/components/builds/render-builds/render-links.js';
+import { renderMetaData } from '/js/components/builds/render-builds/render-metadata.js';
+import { renderOverview } from '/js/components/builds/render-builds/render-content-overview.js';
+import { renderBuildGallery } from '/js/components/builds/render-builds/render-builds-gallery.js';
+
+export function renderVoicekeep() {
+
+  const voicekeepObj = buildsProjectsData.find( project =>
+    project.id === 'voicekeep'
+    );
+
+  const voicekeepContent = voicekeepContents.voicekeep;
+
+  if (!voicekeepObj || voicekeepObj.visibility !== 'public'){
+    return;
+  }
+  
+  const app = document.querySelector('.build-content-container');
+  if (!app) return;
+
+  app.innerHTML = `
+
+    <div class= 'build-page voicekeep-page'>
+
+     ${createBuildHeroMarkup(voicekeepObj)}
+
+     ${renderLinks(voicekeepObj)}
+
+     ${renderBuildGallery(voicekeepScreenshots)}
+
+     ${renderOverview(voicekeepContent)}
+
+     ${renderMetaData(voicekeepObj)}
+
+     
+
+
+    </div>
+
+
+  
+  `; 
+
+  
+}
