5e30a6a58f2224585d6d5700958279aae651ab57
iheymant <Hemant.kaliyar@gmail.com>
2026-08-17 15:41:16 +0530

    move to dedicated path

--- js/pages/builds/voicekeep/voicekeep-page.js
+++ js/pages/builds/voicekeep/voicekeep-page.js
@@ -1,6 +1,6 @@
 import { renderLayout } from "/js/layout/render-layout.js";
 import { initializeLayout } from "/js/layout/initialize-layout.js";
-import { renderVoicekeep } from "/js/components/builds/voicekeep/voicekeep.js";
+import { renderVoicekeep } from "/js/builds/voicekeep/voicekeep.js";
 import { initializeReadingProgress } from "/js/components/reading-progress/setup-reading-progress.js";
 
 
--- js/pages/builds/voicekeep/voicekeep-privacy-page.js
+++ js/pages/builds/voicekeep/voicekeep-privacy-page.js
@@ -1,4 +1,4 @@
-import { renderVoicekeepPrivacy } from "/js/components/builds/voicekeep/voicekeep-privacy.js";
+import { renderVoicekeepPrivacy } from "/js/builds/voicekeep/voicekeep-privacy.js";
 import { initializeReadingProgress } from "/js/components/reading-progress/setup-reading-progress.js";
 import { renderLayout } from "/js/layout/render-layout.js";
 import { initializeLayout } from "/js/layout/initialize-layout.js";
