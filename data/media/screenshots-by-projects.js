3d125d2a156fc152d9286926c7bd56b42074979c
iheymant <Hemant.kaliyar@gmail.com>
2026-08-18 16:28:19 +0530

    create single media file to import other media's paths

---
+++ data/media/screenshots-by-projects.js
@@ -0,0 +1,7 @@
+import { voicekeepScreenshots } from "../builds/voicekeep/screenshots.js";
+import { tracekeepScreenshots } from "../builds/tracekeep/screenshots.js";
+
+export const screenshotsByProject = {
+  voicekeep: voicekeepScreenshots,
+  tracekeep: tracekeepScreenshots,
+};
