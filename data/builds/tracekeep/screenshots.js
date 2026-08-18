b096e1422804fbc60081cfa79366271b6766dcfe
iheymant <Hemant.kaliyar@gmail.com>
2026-08-18 14:58:51 +0530

    add tracekeep media

---
+++ data/builds/tracekeep/screenshots.js
@@ -0,0 +1,38 @@
+export const tracekeepScreenshots = [
+  {
+    image:
+      "/assets/builds/images/tracekeep/screenshots/tracekeep-profile-view.png",
+
+    title: "Get quick overview",
+
+    description: "Quickly get a glance who unfollowed or followed you!.",
+
+    currentFocused: true,
+  },
+
+  {
+    image:
+      "/assets/builds/images/tracekeep/screenshots/tracekeep-detected-changes1.png",
+
+    title: "Never miss a change",
+
+    description: "know about each user in details.",
+  },
+
+  {
+    image:
+      "/assets/builds/images/tracekeep/screenshots/tracekeep-sync-page.png",
+
+    title: "Refresh new unfollowers or followers",
+
+    description: "Automatic sync and detect new changes.",
+  },
+  {
+    image:
+      "/assets/builds/images/tracekeep/screenshots/tracekeep-sync-completed.png",
+
+    title: "Sync completed",
+
+    description: "Every information on your hand.",
+  },
+];
--- js/builds/tracekeep/tracekeep.js
+++ js/builds/tracekeep/tracekeep.js
@@ -1,10 +1,11 @@
 import { buildsProjectsData } from "/data/builds/index.js";
 import { tracekeepContents } from "/data/builds/tracekeep/content.js";
-
+import { tracekeepScreenshots } from "../../../data/builds/tracekeep/screenshots.js";
 import { createBuildHeroMarkup } from "/js/components/builds/render-builds/create-build-hero-markup.js";
 import { renderLinks } from "/js/components/builds/render-builds/render-links.js";
 import { renderMetaData } from "/js/components/builds/render-builds/render-metadata.js";
 import { renderOverview } from "/js/components/builds/render-builds/render-content-overview.js";
+import { renderBuildGallery } from "/js/components/builds/render-builds/render-builds-gallery.js";
 
 export function renderTracekeep() {
   const tracekeepObj = buildsProjectsData.find(
@@ -28,6 +29,7 @@ export function renderTracekeep() {
 
      ${renderLinks(tracekeepObj)}
 
+     ${renderBuildGallery(tracekeepScreenshots)}
 
      ${renderOverview(tracekeepContent)}
 
